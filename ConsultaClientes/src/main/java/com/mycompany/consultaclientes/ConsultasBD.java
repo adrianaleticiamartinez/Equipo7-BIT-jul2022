f0acae4e0276dcb615969939f4d208255c2248a2
Elizabeth-MC <77930876+Elizabeth-MC@users.noreply.github.com>
2022-07-14 11:36:30 -0500

    Update and rename ClaseConsulta.java to ConsultasBD.java

---
+++ ConsultaClientes/src/main/java/com/mycompany/consultaclientes/ConsultasBD.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplohack;
+
+/**
+ *
+ * @author DELL
+ */
+public class ConsultasBD {
+    MySQL baseDatos = new MySQL().conectar("localhost", "reto bbva",
+ "root", "");
+}
